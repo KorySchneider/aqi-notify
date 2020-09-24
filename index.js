0b272d6ddc98fff82c3edcf30e259d5210ad610a
Kory Schneider <kory.schneider@gmail.com>
2020-09-24 00:02:42 -0700

    Send SMS via email with nodemailer with AQI data

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const text = require('textbelt');
+'use strict';
+require('dotenv').config();
 
-text.sendText('5415250440', 'hello world');
+const nodemailer = require('nodemailer');
+const fetch = require('node-fetch');
+const moment = require('moment');
+
+const {
+  SMTP_USER,
+  SMTP_PASS,
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_SECURE,
+  SEND_ADDR,
+  ZIP_CODE,
+  AIR_NOW_API_KEY,
+} = process.env;
+
+const AIR_NOW_URL = `https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&API_KEY=${AIR_NOW_API_KEY}&zipCode=${ZIP_CODE}`;
+
+async function main() {
+  // Get AQI data
+  const aqiResponse = await fetch(AIR_NOW_URL);
+  const aqiData = await aqiResponse.json();
+  if (!aqiData) return;
+
+  // Build message
+  const time = moment(aqiData[0].HourObserved, 'HH').format('h:00 a');
+  const message = `The AQI is ${aqiData[0].AQI} as of ${time}`;
+
+  // Send text via email
+  const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    secure: SMTP_SECURE,
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASS,
+    },
+  });
+  const info = await transporter.sendMail({
+    from: SMTP_USER,
+    to: SEND_ADDR,
+    text: message,
+  });
+  console.log('Message sent:', message, info.messageId);
+}
+
+main().catch(console.error);
